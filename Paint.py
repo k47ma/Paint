d5ef0de83ecd271a8e0535887c1605129876e035
Kai Ma <k47ma@edu.uwaterloo.ca>
2017-07-26 09:50:42 -0400

    - Added color selector

--- Paint.py
+++ Paint.py
@@ -1,10 +1,13 @@
 from tkinter import *
+import tkColorChooser
 
 
 # constants for event status
 SHIFT = 265
 CAPS_LOCK = 266
 CTRL = 268
+
+COLOR = "#000000"
 
 
 # top level class
@@ -38,6 +41,12 @@ class ControlFrame(Frame):
         self.types["pencil"] = pencil_btn
         self.cursors["pencil"] = "ul_angle"
 
+        self.brush_img = PhotoImage(file="image\\brush.gif")
+        brush_img = Button(self, image=self.brush_img, command=lambda: self.select("brush"))
+        brush_img.pack(side=TOP)
+        self.types["brush"] = brush_img
+        self.cursors["brush"] = "target"
+
         self.eraser_img = PhotoImage(file="image\\eraser.gif")
         eraser_btn = Button(self, image=self.eraser_img, command=lambda: self.select("eraser"))
         eraser_btn.pack(side=TOP)
@@ -62,9 +71,13 @@ class ControlFrame(Frame):
         self.types["circle"] = circle_btn
         self.cursors["circle"] = "tcross"
 
+        self.color_img = PhotoImage(file="image\\colors.gif")
+        color_btn = Button(self, image=self.color_img, command=self.selectColor)
+        color_btn.pack(side=TOP, pady=6)
+
         self.clear_img = PhotoImage(file="image\\cross.gif")
         clear_btn = Button(self, image=self.clear_img, command=lambda: self.canvas.clear())
-        clear_btn.pack(side=TOP, pady=5)
+        clear_btn.pack(side=BOTTOM)
 
         self.select("pencil")
 
@@ -76,6 +89,12 @@ class ControlFrame(Frame):
         self.types[name]["relief"] = SUNKEN
         self.canvas["cursor"] = self.cursors[name]
         self.canvas.type = name
+
+    def selectColor(self):
+        global COLOR
+        selected_color = tkColorChooser.askcolor(COLOR, parent=self, title="Color Selector")
+        if selected_color[1]:
+            COLOR = selected_color[1]
 
 
 # frame for canvas
@@ -102,17 +121,20 @@ class PaintCanvas(Canvas):
             self.delete(self.lastDraw)
 
         if self.type == "pencil":
-            self.create_line((self.lastX, self.lastY, event.x, event.y), activefill="blue")
+            self.create_line((self.lastX, self.lastY, event.x, event.y), fill=COLOR)
             self.lastX = event.x
             self.lastY = event.y
+        elif self.type == "brush":
+            r = 7.5
+            self.create_oval((event.x - r, event.y - r, event.x + r, event.y + r), fill=COLOR, outline=COLOR)
         elif self.type == "eraser":
             r = 20.0
             self.create_oval((event.x - r, event.y - r, event.x + r, event.y + r), fill="white", outline="white")
         elif self.type == "line":
             if event.state == SHIFT:
-                self.lastDraw = self.create_line((self.lastX, self.lastY, event.x, event.y))
+                self.lastDraw = self.create_line((self.lastX, self.lastY, event.x, event.y), fill=COLOR)
             else:
-                self.lastDraw = self.create_line((self.lastX, self.lastY, event.x, event.y))
+                self.lastDraw = self.create_line((self.lastX, self.lastY, event.x, event.y), fill=COLOR)
         elif self.type == "rect":
             if event.state == SHIFT:
                 side = min(abs(self.lastX - event.x), abs(self.lastY - event.y))
@@ -131,10 +153,10 @@ class PaintCanvas(Canvas):
                 # draw square
                 end_x = self.lastX + dir_x * side
                 end_y = self.lastY + dir_y * side
-                self.lastDraw = self.create_rectangle(self.lastX, self.lastY, end_x, end_y)
+                self.lastDraw = self.create_rectangle(self.lastX, self.lastY, end_x, end_y, outline=COLOR)
             else:
                 #draw rectangle
-                self.lastDraw = self.create_rectangle(self.lastX, self.lastY, event.x, event.y)
+                self.lastDraw = self.create_rectangle(self.lastX, self.lastY, event.x, event.y, outline=COLOR)
         elif self.type == "circle":
             if event.state == SHIFT:
                 diameter = min(abs(self.lastX - event.x), abs(self.lastY - event.y))
@@ -153,9 +175,9 @@ class PaintCanvas(Canvas):
                 # draw circle
                 end_x = self.lastX + dir_x * diameter
                 end_y = self.lastY + dir_y * diameter
-                self.lastDraw = self.create_oval((self.lastX, self.lastY, end_x, end_y))
+                self.lastDraw = self.create_oval((self.lastX, self.lastY, end_x, end_y), outline=COLOR)
             else:
-                self.lastDraw = self.create_oval((self.lastX, self.lastY, event.x, event.y))
+                self.lastDraw = self.create_oval((self.lastX, self.lastY, event.x, event.y), outline=COLOR)
 
     def setDraw(self, event):
         self.firstClick = True
