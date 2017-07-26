51f378dd70e641ad26382dd7ea8028f21bdf0c7f
Kai Ma <k47ma@edu.uwaterloo.ca>
2017-07-26 12:46:44 -0400

    - Added tooltip for all the buttons

--- Tooltip.py
+++ Tooltip.py
@@ -1,5 +1,3 @@
-from threading import Thread
-from time import sleep
 from tkinter import Toplevel, Label
 
 
@@ -7,47 +5,22 @@ class Tooltip(object):
     def __init__(self, widget, text):
         self.widget = widget
         self.text = text
-        self.thread = None
         self.root = None
-        self.widget.bind("<Enter>", self.tooltip_thread_start)
-        self.widget.bind("<Leave>", self.destroy_tooltip)
+        self.widget.bind("<Enter>", self.enter)
+        self.widget.bind("<Leave>", self.leave)
 
-    def tooltip_thread_start(self, event):
-        self.thread = TooltipThread(self.widget, self.text, self)
-        self.thread.daemon = True
-        self.thread.run()
-
-    def tooltip_thread_stop(self):
-        self.thread.cancelled = True
-
-    def destroy_tooltip(self, event):
-        if self.root:
-            self.root.destroy()
-
-
-class TooltipThread(Thread):
-    def __init__(self, widget, text, parent):
-        Thread.__init__(self)
-
-        self.widget = widget
-        self.text = text
-        self.parent = parent
-        self.cancelled = False
-
-    def run(self):
-        sleep(0.6)
-
-        if not self.cancelled:
-            self.create_tooltip()
-
-    def create_tooltip(self):
+    def enter(self, event):
         x, y, cx, cy = self.widget.bbox()
         x += self.widget.winfo_rootx() + 25
-        y += self.widget.winfo_rooty() + 20
+        y += self.widget.winfo_rooty() + 3
 
-        self.parent.root = Toplevel(self.widget)
-        self.parent.root.wm_overrideredirect(True)
-        self.parent.root.wm_geometry("+%d+%d" % (x, y))
+        self.root = Toplevel(self.widget)
+        self.root.wm_overrideredirect(True)
+        self.root.wm_geometry("+%d+%d" % (x, y))
 
-        label = Label(self.parent.root, text=self.text, justify="left", bg="yellow", relief="solid", bd=1, font=("time", 9))
+        label = Label(self.root, text=self.text, justify="left", bg="white", relief="solid", bd=1, font=("time", 9))
         label.pack()
+
+    def leave(self, event):
+        if self.root:
+            self.root.destroy()
