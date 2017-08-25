93087b014c5c1704ef7ee9cd0c28a3f862934b0b
Kai Ma <k47ma@edu.uwaterloo.ca>
2017-08-25 10:40:53 -0400

    - Added gradient feature to color

--- color.py
+++ color.py
@@ -1,21 +1,43 @@
 import Tkinter
 import random
 import threading
+import time
 
 
 class App:
     def __init__(self, t):
+        self.status = False
+        self.reverse = False
+        self.r = random.randint(0, 255)
+        self.g = random.randint(0, 255)
+        self.b = random.randint(0, 255)
         self.image = self.generate_image()
+
         self.c = Tkinter.Canvas(t, width=100, height=100)
         self.c.pack()
         self.i = self.c.create_image(0, 0, image=self.image, anchor=Tkinter.NW)
 
-        btn = Tkinter.Button(t, text="start", command=self.start)
-        btn.pack()
+        self.btn = Tkinter.Button(t, text="start", command=self.start)
+        self.btn.pack()
 
-    def generate_image(self):
-        image = Tkinter.PhotoImage(width=100, height=100)
-        colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for j in range(0, 10000)]
+    def generate_image(self, image=None):
+        if not image:
+            image = Tkinter.PhotoImage(width=100, height=100)
+        #self.r = random.randint(0, 255)
+        self.g = random.randint(0, 255)
+        self.b = random.randint(0, 255)
+        if not self.reverse:
+            self.r += 2
+        else:
+            self.r -= 2
+
+        if self.r > 255:
+            self.r = 255
+            self.reverse = True
+        elif self.r < 0:
+            self.r = 0
+            self.reverse = False
+        colors = [(self.r, self.r, self.r) for j in range(0, 10000)]
         row = 0
         col = 0
         for color in colors:
@@ -27,9 +49,19 @@ class App:
         return image
 
     def start(self, *args):
+        self.status = True
+
         thread = self.MyThread(self, self.c, self.i)
         thread.setDaemon(True)
         thread.start()
+
+        self.btn["command"] = self.stop
+        self.btn["text"] = "stop"
+
+    def stop(self):
+        self.status = False
+        self.btn["command"] = self.start
+        self.btn["text"] = "start"
 
     class MyThread(threading.Thread):
         def __init__(self, parent, canvas, image):
@@ -39,9 +71,10 @@ class App:
             self.i = image
 
         def run(self):
-            while True:
-                self.image = self.parent.generate_image()
-                self.c.itemconfigure(self.i, image=self.image)
+            while self.parent.status:
+                self.parent.image = self.parent.generate_image()
+                self.c.itemconfigure(self.i, image=self.parent.image)
+
 
 t = Tkinter.Tk()
 a = App(t)
