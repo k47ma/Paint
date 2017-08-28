da91ad1058da19f7eaa485089fa1cdb21f0590ce
Kai Ma <k47ma@edu.uwaterloo.ca>
2017-08-28 11:02:18 -0400

    - Fixed bugs in color

--- color.py
+++ color.py
@@ -1,6 +1,5 @@
 import Tkinter
 import random
-import threading
 import time
 
 
@@ -13,8 +12,9 @@ class App(Tkinter.Tk):
         self.r = random.randint(0, 255)
         self.g = random.randint(0, 255)
         self.b = random.randint(0, 255)
-        self.x = 0
-        self.y = 0
+        self.x = random.randint(0, 99)
+        self.y = random.randint(0, 99)
+        self.boat = Tkinter.PhotoImage(file="image\\boat.gif")
         self.origin_x = 50
         self.origin_y = 50
         self.image = self.generate_image()
@@ -22,21 +22,26 @@ class App(Tkinter.Tk):
         self.c = Tkinter.Canvas(self, width=100, height=100)
         self.c.pack()
         self.i = self.c.create_image(0, 0, image=self.image, anchor=Tkinter.NW)
+        self.boat_position = self.c.create_image(self.x, self.y, image=self.boat, anchor=Tkinter.CENTER)
 
-        self.btn = Tkinter.Button(self, text="start", command=self.start)
-        self.btn.pack()
+        self.btn = Tkinter.Button(self, width=6, text="start", command=self.start)
+        self.btn.pack(pady=5)
 
         self.bind("<Up>", self.move_up)
-        self.bind("<Down>", self.move_dowm)
+        self.bind("<Down>", self.move_down)
         self.bind("<Left>", self.move_left)
         self.bind("<Right>", self.move_right)
         self.c.bind("<Button-1>", self.set_origin)
+        self.c.bind("<B1-Motion>", self.set_origin)
+        self.bind("<Return>", self.check_position)
 
-    def move_up(self, event):
+        self.update_image()
+
+    def move_down(self, event):
         if self.origin_y < 99:
             self.origin_y += 1
 
-    def move_dowm(self, event):
+    def move_up(self, event):
         if self.origin_y > 2:
             self.origin_y -= 1
 
@@ -49,7 +54,19 @@ class App(Tkinter.Tk):
             self.origin_x += 1
 
     def set_origin(self, event):
-        self.origin = event.y
+        self.origin_x = event.x
+        self.origin_y = event.y
+
+    def check_position(self, event):
+        if self.origin_x - 5 <= self.x <= self.origin_x + 5 and self.origin_y - 5 <= self.y <= self.origin_y + 5:
+            # delete the old boat and generate a new one
+            self.c.delete(self.boat_position)
+            self.x = random.randint(0, 99)
+            self.y = random.randint(0, 99)
+            self.boat_position = self.c.create_image(self.x, self.y, image=self.boat, anchor=Tkinter.CENTER)
+            print "Target eliminated!"
+        else:
+            print "Target missed!"
 
     def generate_image(self):
         image = Tkinter.PhotoImage(width=100, height=100)
@@ -64,7 +81,7 @@ class App(Tkinter.Tk):
                 row = 0
         return image
 
-    def update_image(self, image=None):
+    def update_image(self):
         image = self.image.copy()
         #self.r = random.randint(0, 255)
         self.g = random.randint(0, 255)
@@ -72,33 +89,38 @@ class App(Tkinter.Tk):
 
         color = (self.r, self.r, self.r)
 
-        self.x += 1
-        if self.x == 100:
-            self.x = 0
+        if self.reverse:
+            self.r -= 2
+        else:
+            self.r += 2
 
-            if self.reverse:
-                self.r -= 5
-            else:
-                self.r += 5
+        if self.r > 255:
+            self.r = 255
+            self.reverse = True
+        elif self.r < 0:
+            self.r = 0
+            self.reverse = False
 
-            if self.r > 255:
-                self.r = 255
-                self.reverse = True
-            elif self.r < 0:
-                self.r = 0
-                self.reverse = False
-
-        for y in range(100):
-            image.put("#%02x%02x%02x" % color, (self.x, y))
+        for x in range(100):
+            for y in range(100):
+                if (x == self.origin_x - 5 or x == self.origin_x + 5) and self.origin_y - 5 <= y <= self.origin_y + 5:
+                    image.put("#00ff00", (x, y))
+                elif (y == self.origin_y - 5 or y == self.origin_y + 5) and self.origin_x - 5 <= x <= self.origin_x + 5:
+                    image.put("#00ff00", (x, y))
+                elif x == self.origin_x or y == self.origin_y:
+                    image.put("#00ff00", (x, y))
+                else:
+                    image.put("#%02x%02x%02x" % color, (x, y))
 
         self.image = image
+        self.c.itemconfigure(self.i, image=self.image)
+
+        if self.status:
+            self.after(1, self.update_image)
 
     def start(self, *args):
         self.status = True
-
-        thread = self.MyThread(self, self.c, self.i)
-        thread.setDaemon(True)
-        thread.start()
+        self.update_image()
 
         self.btn["command"] = self.stop
         self.btn["text"] = "stop"
@@ -107,19 +129,6 @@ class App(Tkinter.Tk):
         self.status = False
         self.btn["command"] = self.start
         self.btn["text"] = "start"
-
-    class MyThread(threading.Thread):
-        def __init__(self, parent, canvas, image):
-            threading.Thread.__init__(self)
-            self.parent = parent
-            self.c = canvas
-            self.i = image
-
-        def run(self):
-            while self.parent.status:
-                self.parent.update_image()
-                self.c.itemconfigure(self.i, image=self.parent.image)
-                time.sleep(0.01)
 
 
 a = App()
